ec3096be92711a8439d5035fa3ef520958823fe7
celestial-ux <helgatabitha2@gmail.com>
2024-09-09 16:13:13 +0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,22 @@
+package main 
+
+import "fmt"
+
+func main() {
+	var nama string = "Helga Tabitha"
+	var umur int = 18
+	var alamat string = "Permata timur 2 blok rr 5"
+	var berkacamata bool = true
+	var minuskiri float32 = 0.50
+	var minuskanan float32 = 0.50
+	
+	var hasilminus float32 = minus kiri + minus kanan
+	
+	fmt.Println(nama)
+	fmt.Println(umur)
+	fmt.Println(alamat)
+	fmt.Println(berkacamata)
+	fmt.Println(minuskiri)
+	fmt.Println(minuskanan)
+	fmt.Println(hasilminus)
+}
